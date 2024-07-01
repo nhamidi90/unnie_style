fa0c2435d1d04bcc6d4a477aef5b8298fef7e375
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-01 13:01:14 +0000

    Add bag context and delivery logic

--- unnie_style/settings.py
+++ unnie_style/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -158,3 +159,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 40
+STANDARD_DELIVERY = 3.99
