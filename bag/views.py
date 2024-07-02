bab8831654619f4304379b2a8a629ce58977a581
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-02 12:20:20 +0000

    Add product size logic

--- bag/views.py
+++ bag/views.py
@@ -12,11 +12,29 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    size = None
+    color = None
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    if 'product_color' in request.POST:
+        color = request.POST['product_color']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
