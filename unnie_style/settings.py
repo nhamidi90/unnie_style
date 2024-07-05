f21af7fcd9b45a838da2058d774200c49310188e
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-05 11:50:24 +0000

    Add basic checkout functionality

--- unnie_style/settings.py
+++ unnie_style/settings.py
@@ -172,5 +172,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# stripe
 FREE_DELIVERY_THRESHOLD = 40
 STANDARD_DELIVERY = 3.99
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
