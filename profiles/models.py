5d4a0f995056718ce47e61da7efd5464ba926387
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-08 11:53:36 +0000

    Create profiles app

---
+++ profiles/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django_countries.fields import CountryField
+
+
+# Create your models here.
+class UserProfile(models.Model):
+    """
+    User profile model
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """ Create or update user profile"""
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
+
+
+class Address(models.Model):
+    """
+    Model for addresses
+    """
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_address = models.BooleanField(default=False, null=True, blank=True)
