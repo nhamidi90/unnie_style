94794b3930d99db9ef236df87c7529a1a0c13b82
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-08 11:12:27 +0000

    Update form to change county to CountryField

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from products.models import Product
 
 # Create your models here.
@@ -19,7 +21,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, 
                                       null=False, default=0)
