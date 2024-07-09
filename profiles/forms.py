c70caba7affae843c9dbae4263c73067cb49696c
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-09 11:11:23 +0000

    Add 'add address' form to profile

---
+++ profiles/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Addresses
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Addresses
+        fields = ('street_address1', 'street_address2', 'town_or_city', 
+                  'county', 'postcode', 'country')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County',
+        }
+
+        self.fields['street_address1'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import AddressForm
 
 
 # Create your views here.
 def profile(request):
     """ Display user's profile """
-
+    form = AddressForm()    
     profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
 
     context = {
         'profile': profile,
+        'form': form,
     }
     return render(request, template, context)
