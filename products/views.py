42d7b2edb1baf43189a92d7324a40149a49e1e50
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-11 10:22:18 +0000

    Add edit product views and templates

--- products/views.py
+++ products/views.py
@@ -80,7 +80,7 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully addes product')
+            messages.success(request, 'Successfully added product')
             return redirect(reverse('add_product'))
         else:
             messages.error(request, 'Product could not be added. Please make \
@@ -93,4 +93,30 @@ def add_product(request):
         'form': form,
     }
 
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product """
+    
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully updated product')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Product could not be updated. Please make \
+                sure the form is valid')
+    else:
+        form = ProductForm(instance=product)
+
+    template = 'products/edit_product.html'
+
+    context = {
+        'form': form,
+        'product': product,
+    }       
     return render(request, template, context)
