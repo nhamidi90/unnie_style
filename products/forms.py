a6c711b1bed64e0b0516bec727d16cc3c530498f
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-11 13:32:04 +0000

    Fix the image field for adding/editing products

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -8,9 +9,12 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = "__all__"
 
+    main_image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_name = [{c.id, c.get_friendly_name()} for c in categories]
+        friendly_name = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_name
+        
