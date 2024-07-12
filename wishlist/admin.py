030a37b0bc35d68437c4fd92c7d1156485b0fe0b
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-12 14:01:55 +0000

    Add admin view

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Wishlist
 
 # Register your models here.
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_profile',
+        'products',
+    )
+
+admin.site.register(Wishlist, WishlistAdmin)
