5058365b38af28fee25bb2018213542cad9ae572
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-12 13:52:22 +0000

    Setup wishlist models, views, urls, template

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
 
 # Create your models here.
+class Wishlist(models.Model):
+    """Wishlist model"""
+    products = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(UserProfile, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def wishlist(request):
+    """ Display the user's wishlist"""
+    template = "wishlist/wishlist.html"
+    context = {
+
+    }
+    return render(request, template, context)
