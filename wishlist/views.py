a1e13262d3aea3aa6440f4d097a050ef4492d15c
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-13 13:10:46 +0000

    Add wishlist products to wishlist page

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Wishlist
+from profiles.models import UserProfile
 
 # Create your views here.
 def wishlist(request):
     """ Display the user's wishlist"""
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.filter(user_profile=profile)
+
     template = "wishlist/wishlist.html"
     context = {
-
+        'profile': profile,
+        'wishlist': wishlist,
     }
     return render(request, template, context)
