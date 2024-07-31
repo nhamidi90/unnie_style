4c4a6c02b64ca5c5b157da2ddd157c3940649a08
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-31 11:26:29 +0000

    Remove secret key and set debug

--- unnie_style/settings.py
+++ unnie_style/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b3+8+ev)w#-xii$vf!95-@!m1#&q03g_m^4rt-!5hr)+=wk!r#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-nhamidi90-unniestyle-ne47i6fjyzl.ws.codeinstitute-ide.net',
                  'localhost',
