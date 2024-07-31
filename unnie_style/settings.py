389537911b6dd88418a980460a3acdc34e5355a0
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-07-31 20:50:03 +0000

    Fix typo for email backend

--- unnie_style/settings.py
+++ unnie_style/settings.py
@@ -216,7 +216,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'customerservice@unniestyle.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.Emailbackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
