2b80270dddf071d68fb403677b600c5545d19103
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-08-01 09:14:34 +0000

    Change debug to solve server problem

--- unnie_style/settings.py
+++ unnie_style/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['8000-nhamidi90-unniestyle-ne47i6fjyzl.ws.codeinstitute-ide.net',
                  'localhost',
