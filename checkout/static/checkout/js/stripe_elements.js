814db8a071aee9d4ca9ad2ed97e76e907c7f411e
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-08-02 11:42:57 +0000

    Add semicolons for validation

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -52,7 +52,7 @@ form.addEventListener('submit', function(ev) {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
         'save_info': saveInfo,
-    }
+    };
     var url = '/checkout/cache_checkout_data/';
 
     $.post(url, postData).done(function () {
@@ -105,5 +105,5 @@ form.addEventListener('submit', function(ev) {
         });
     }).fail(function () {
         location.reload();
-    })
+    });
 });
