de50a8626c364828c44b6b0f2773aaabfe6b0a91
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-08-11 10:19:56 +0000

    Remove commas

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -69,13 +69,13 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.phone_number = shipping_details.phone,
-                profile.street_address1 = shipping_details.address.line1,
-                profile.street_address2 = shipping_details.address.line2,
-                profile.town_or_city = shipping_details.address.city,
-                profile.county = shipping_details.address.state,
-                profile.postcode = shipping_details.address.postal_code,
-                profile.country = shipping_details.address.country,
+                profile.phone_number = shipping_details.phone
+                profile.street_address1 = shipping_details.address.line1
+                profile.street_address2 = shipping_details.address.line2
+                profile.town_or_city = shipping_details.address.city
+                profile.county = shipping_details.address.state
+                profile.postcode = shipping_details.address.postal_code
+                profile.country = shipping_details.address.country
                 profile.save()
 
         order_exists = False
