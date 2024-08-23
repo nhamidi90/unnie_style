8cc2ed50383aacf8359c1813944c87ab1bdcddca
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-08-23 20:03:51 +0000

    Create custom 404/500 page

--- unnie_style/urls.py
+++ unnie_style/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'unnie_style.views.handler404'
+handler500 = 'unnie_style.views.handler500'
